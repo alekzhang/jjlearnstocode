4a5a463d61d9afd1f59a729835a1829e1e2ac15e
Alek <alekzhang@gmail.com>
2015-04-15 20:03:55 -0700

    babys first commit

---
+++ jjlearnstocode/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jjlearnstocode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HistoricalStockData> AAPL = DownloadData("AAPL");
+            Console.ReadLine();
+        }
+
+        public static List<HistoricalStockData> DownloadData(string ticker)
+        {
+            List<HistoricalStockData> retval = new List<HistoricalStockData>();
+
+            using (WebClient web = new WebClient())
+            {
+                string data = web.DownloadString(string.Format("http://ichart.finance.yahoo.com/table.csv?s={0}", ticker));
+
+                data = data.Replace("r", "");
+
+                string[] rows = data.Split('\n');
+
+                //First row is headers so Ignore it
+                for (int i = 1; i < rows.Length; i++)
+                {
+                    if (rows[i].Replace("n", "").Trim() == "") continue;
+
+                    string[] cols = rows[i].Split(',');
+
+                    HistoricalStockData hs = new HistoricalStockData();
+                    hs.Date = Convert.ToDateTime(cols[0]);
+                    hs.Open = Convert.ToDouble(cols[1]);
+                    hs.High = Convert.ToDouble(cols[2]);
+                    hs.Low = Convert.ToDouble(cols[3]);
+                    hs.Close = Convert.ToDouble(cols[4]);
+                    hs.Volume = Convert.ToDouble(cols[5]);
+                    hs.AdjClose = Convert.ToDouble(cols[6]);
+
+                    retval.Add(hs);
+                }
+
+                return retval;
+            }
+        }
+    }
+}
